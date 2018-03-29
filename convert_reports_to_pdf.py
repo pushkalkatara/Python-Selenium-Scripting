46107092cd2809d2f170f68bd36aa62b79ad3339
issamrasheed <37844722+issamrasheed@users.noreply.github.com>
2018-03-29 09:15:47 +0530

    Convert all  eLab reports into a single PDF
    This tool asks for the location of the folder in which all eLab reports are saved and produces a PDF containing the reports, with the same name as the folder.

---
+++ convert_reports_to_pdf.py
@@ -0,0 +1,36 @@
+"""
+A tool to convert all eLab reports into a single PDF.
+
+Requirements:
+
+Python == 3.6.3
+img2pdf == 0.2.4
+
+Instructions:
+1. Go to the folder that holds all your eLab reports. Right-click on any report, go to Properties
+and copy the Location to 'path_to_dir' as shown below.
+2. Run this program.
+3. A PDF with the same name as the folder will be created with all the generated reports in ordered form. 
+"""
+
+import os
+import img2pdf
+from os import path
+
+def printpdf(out_filename,path_to_dir):
+    os.chdir(path_to_dir)
+    if path.exists('report.png'):
+        os.rename('report.png', 'report (0).png')
+    for j in range(0,10):
+        if path.exists(f'report ({str(j)}).png'):
+            os.rename(f'report ({str(j)}).png', f'report (0{str(j)}).png')
+
+    with open(out_filename+'.pdf', "wb") as f:
+        f.write(img2pdf.convert([i for i in os.listdir(path_to_dir) if i.endswith(".png")]))
+
+#Go to the folder that holds all your eLab reports. Right-click on any report, go to Properties
+#and copy the Location to 'path_to_dir' as shown below
+path_to_dir = r"C:\Users\Issam\Desktop\ELAB\issamada"  
+out_filename = path_to_dir.split('\\')[-1]
+
+printpdf(out_filename,path_to_dir)
